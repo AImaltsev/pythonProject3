e1f76050e25d0da0dfb46a25b924ed9357b44750
AImaltsev <boomer47.am@gmail.com>
2023-09-03 16:43:52 +0400

    Update sorted.py

--- task/sorted.py
+++ task/sorted.py
@@ -1,12 +1,23 @@
-import math
+import random
 
-# Вычисляем 100!
-factorial_100 = math.factorial(100)
+#
 
-# Преобразуем значение в строку
-factorial_str = str(factorial_100)
+def qsort(array, left, right):
+    middle = (left + right) // 2
 
-# Подсчитываем количество символов
-num_digits = len(factorial_str)
+    p = random.choice(array[left: right + 1])
+    i, j = left, right
+    while i <= j:
+        while array[i] < p:
+            i += 1
+        while array[j] > p:
+            j -= 1
+        if i <= j:
+            array[i], array[j] = array[j], array[i]
+            i += 1
+            j -= 1
 
-print("Количество цифр в 100!:", num_digits)
+    if j > left:
+        qsort(array, left, j)
+    if right > i:
+        qsort(array, i, right)
