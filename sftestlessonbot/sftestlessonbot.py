7a213e9c66df7cd9e9b321f0bc44bd57b7a7559a
AImaltsev <boomer47.am@gmail.com>
2023-09-10 13:57:17 +0400

    Update sftestlessonbot.py

--- sftestlessonbot/sftestlessonbot.py
+++ sftestlessonbot/sftestlessonbot.py
@@ -3,3 +3,12 @@ import telebot
 TOKEN = "6379299948:AAGcv7L5KIyOKwyGA2Cy1kksDoUT-cDcmqM"
 
 bot = telebot.TeleBot(TOKEN)
+
+
+# Обрабатываются все сообщения, содержащие команды '/start' or '/help'.
+@bot.message_handler(commands=['start', 'help'])
+def repeat(message: telebot.types.Message):
+    bot.reply_to(message, f"Привет, {message.chat.username}")
+
+
+bot.polling(none_stop=True)
